20791a49aa9089ed7c87669084b7572e786bf03d
crash2807 <57901862+crash2807@users.noreply.github.com>
2020-09-02 22:22:49 +0200

    Create GiftsController.cs
    dodanie kontrolera giftow

---
+++ ProjektInzynierski/ProjektInzynierski/Controllers/GiftsController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProjektInzynierski.Models;
+
+namespace ProjektInzynierski.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GiftsController : ControllerBase
+    {
+        private readonly s17041Context _context;
+
+        public GiftsController(s17041Context context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Gifts
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Gift>>> GetGift()
+        {
+            return await _context.Gift.ToListAsync();
+        }
+
+        // GET: api/Gifts/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Gift>> GetGift(int id)
+        {
+            var gift = await _context.Gift.FindAsync(id);
+
+            if (gift == null)
+            {
+                return NotFound();
+            }
+
+            return gift;
+        }
+
+        // PUT: api/Gifts/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutGift(int id, Gift gift)
+        {
+            if (id != gift.IdGift)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(gift).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!GiftExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Gifts
+        [HttpPost]
+        public async Task<ActionResult<Gift>> PostGift(Gift gift)
+        {
+            _context.Gift.Add(gift);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (GiftExists(gift.IdGift))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetGift", new { id = gift.IdGift }, gift);
+        }
+
+        // DELETE: api/Gifts/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Gift>> DeleteGift(int id)
+        {
+            var gift = await _context.Gift.FindAsync(id);
+            if (gift == null)
+            {
+                return NotFound();
+            }
+
+            _context.Gift.Remove(gift);
+            await _context.SaveChangesAsync();
+
+            return gift;
+        }
+
+        private bool GiftExists(int id)
+        {
+            return _context.Gift.Any(e => e.IdGift == id);
+        }
+    }
+}
